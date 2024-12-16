099ec75c8af9fa03d5e98aa9c61948b5a19aa9d3
jptx.aya <jptx.aya@gmail.com>
2024-12-16 13:44:05 +0100

    Puzzle10 P2

--- puzzle10.py
+++ puzzle10.py
@@ -1,7 +1,7 @@
 def find_trails(pos_i, pos_j, next_number, finded_finished):
     if (pos_i >= 0) and (pos_i < len(topo)) and (pos_j >= 0) and (pos_j < len(topo[0])):
         if next_number == 9 and int(topo[pos_i][pos_j]) == 9:
-            finded_finished.add((pos_i,pos_j))
+            finded_finished.append((pos_i,pos_j))
         else: 
             if int(topo[pos_i][pos_j]) == next_number:
                 #Find in al direction the next number
@@ -26,7 +26,7 @@ while (i < len(topo)):
     j = 0
     while( j < len(topo[0])):
         if topo[i][j] == "0":
-            finded_finished = set()
+            finded_finished = []
             find_trails( i , j, 0, finded_finished)
             trails += len(finded_finished)
         j += 1
