59648968a460742054d4003c67ca7ff048e420c4
jptx.aya <jptx.aya@gmail.com>
2024-12-17 21:18:27 +0100

    Puzzle13 P2

--- puzzle13.py
+++ puzzle13.py
@@ -1,11 +1,13 @@
 import numpy as np
-def using_numpy(a,b, r):
+
+def using_numpy(a,b,r):
     e1 = np.array([[int(a[0]), int(b[0])],[int(a[1]), int(b[1])]])
-    s = np.array([int(r[0]), int(r[1])])
-    result = np.linalg.solve(e1,s)
-    if (result[0] % 1 == 0) and  (result[0] / 1 > 0) and (result[1] % 1 == 0) and  (result[1] / 1 > 0):
+    s = np.array([int(r[0]), int(r[1])]) + 10000000000000
+    result = np.linalg.solve(e1,s).round()
+    if (int(a[0]) * int(result[0]) + int(b[0]) * int(result[1]) == int(s[0])) and (int(a[1]) * int(result[0]) + int(b[1]) * int(result[1]) == int(s[1])):
         #Problem with decimals
         return result[0] * 3 + result[1]
+    return 0
 
 def using_loops(a,b,r):
     result_a = 0
@@ -33,6 +35,7 @@ while (k < len(operations)):
     b = line2.replace("Button B: X+","").replace(", Y+"," ").split(" ")
     r = line3.replace("Prize: X=","").replace(", Y="," ").split(" ")
     result_a, result_b = using_loops(a,b,r)
-    sum_tokens += 3 * result_a + result_b 
+    #sum_tokens += 3 * result_a + result_b 
+    sum_tokens += using_numpy(a,b,r)
     k += 4
 print(sum_tokens)
