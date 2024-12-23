8ae332e292b754204122005ed50dcdbb970f589c
jptx.aya <jptx.aya@gmail.com>
2024-12-23 12:11:52 +0100

    Puzzle21 P2 v0.1

--- puzzle21.py
+++ puzzle21.py
@@ -22,13 +22,11 @@ remote_pad = {
     "v":{">":">","^":"^","<":"<"},
     "<":{"v":">"}
 }
-
 def get_shorted(elements):
     result = []
     minimun_length= min([len(x) for x in elements])
     result = list(filter(lambda x: len(x) == minimun_length,elements))
     return result
-
 
 def djistra(pad,start,end):
     pq = []
@@ -51,6 +49,18 @@ def djistra(pad,start,end):
             next_path += pad[node][elem]
             heapq.heappush(pq,(len(next_path),elem,next_path))
     return routes
+
+remote_pad_dict = dict()
+keys =["A","<",">","v","^"]
+for index in keys:
+    remote_pad_dict[index] = dict()
+    for end in list(filter(lambda x: x != index,keys)):
+        routes = []
+        routes = djistra(remote_pad,index,end)
+        routes = get_shorted(routes)
+        remote_pad_dict[index][end] = routes
+    remote_pad_dict[index][index] = ["A"]
+
 
 def convert_number(sequence):
     return int(sequence.replace("A", ""))
@@ -81,6 +91,7 @@ def get_sum_code(sequence):
     for elem in second_robot:
         aux = [""]
         for l_elem in list(elem):
+            print(start,l_elem)
             rts = djistra(remote_pad,start,l_elem)
             rts = get_shorted(rts)
             aux = [ x+y for x in aux for y in rts]
@@ -92,14 +103,50 @@ def get_sum_code(sequence):
     converted_number = convert_number(sequence)
     return length_code * converted_number
 
+def get_sum_code2(sequence):
+    code = list(sequence)
+    first_robot = []
+    start = "A"
+    first_robot = [""]
+    for elem in code:
+        rts = djistra(keypad,start,elem)
+        rts = get_shorted(rts)
+        first_robot = [ x+y for x in first_robot for y in rts]
+        start = elem
+    num_robots = 2
+    robot = first_robot
+    result_robot = []
+    for num_robot in range(num_robots):
+        print(f"Robot:{num_robot}")
+        aux_robot = []
+        for elem in robot:
+            aux = [""]
+            start = "A"
+            for l_elem in list(elem):
+                rts = remote_pad_dict[start][l_elem]
+                aux = [ x+y for x in aux for y in rts]
+                start = l_elem
+            aux_robot.extend(aux)
+            aux_robot = get_shorted(aux_robot)
+        robot = aux_robot
+        robot = get_shorted(aux_robot)
+        robot = robot
+    result_robot.extend(robot)
+    result_robot = get_shorted(result_robot)
+    length_code = len(result_robot[0])
+    print(f"Sequence {sequence}:{length_code}")
+    converted_number = convert_number(sequence)
+    return length_code * converted_number
+
+
 #Testing
-#test = get_sum_code("980A")
+#test = get_sum_code2("980A")
 
 
-with open("data/data21.txt") as file:
+with open("data/data21_test.txt") as file:
     sum_codes = 0
     for elem in file.read().splitlines():
-        sum_codes += get_sum_code(elem)
+        sum_codes += get_sum_code2(elem)
 
 print(f"Result:{sum_codes}")
 
